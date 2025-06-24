54a5f35a6a6eb9778090d2d047c0d73e7f60ffa8
Eniot <sunather25@gmail.com>
2025-06-24 18:28:03 +0900

    change the path in main.py to make the link between the script and the index.html

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import StreamingResponse
+from fastapi.responses import StreamingResponse, HTMLResponse
 import io
 from PIL import Image
-import background_generation  # ton script
+import background_generation  # mon script
+import os
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    with open("index.html") as f:
-        return f.read()
+    current_dir = os.path.dirname(__file__)
+    file_path = os.path.join(current_dir, "../../../frontend/index.html")
+    with open(file_path) as f:
+        return HTMLResponse(content=f.read())
 
 @app.post("/process")
 async def process(image: UploadFile = File(...)):
@@ -17,7 +20,7 @@ async def process(image: UploadFile = File(...)):
 
     # Appel de ton script pour générer l'image
     result_img = background_generation.process(img)  # adapte à ta fonction
-    
+
     buf = io.BytesIO()
     result_img.save(buf, format="JPEG")
     buf.seek(0)
