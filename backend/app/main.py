c985b72d8fc636c793b273db6553f6ed6744040a
Eniot <sunather25@gmail.com>
2025-06-26 18:54:34 +0900

    FastAPI now working

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Monter les fichiers statiques
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
+# Dossier des templates HTML
+templates = Jinja2Templates(directory="frontend")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
